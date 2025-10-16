fbfe83413189372f4daa23a062d6a0e193b2eace
Ryan Fernandes Bertaglia <ryanfernandesbertaglia@gmail.com>
2025-10-16 20:45:08 -0300

    fix access on TLS

---
+++ Estoque/src/test/java/RunApplication.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RunApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RunApplication.class, args);
+    }
+}
