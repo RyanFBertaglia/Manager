ef6abedc2d9eda4e71fdb5d67a018d9bfa9729f0
Ryan Fernandes Bertaglia <ryanfernandesbertaglia@gmail.com>
2025-11-16 22:04:51 -0300

    grpc port estoque

--- Estoque/src/main/java/com/manager/grpc/server/EstoqueGrpcService.java
+++ Estoque/src/main/java/com/manager/grpc/server/EstoqueGrpcService.java
@@ -1,59 +1,40 @@
 package com.manager.grpc.server;
 
+import com.manager.model.Product;
+import com.manager.service.EstoqueService;
 import io.grpc.stub.StreamObserver;
-import lombok.Getter;
-import lombok.Setter;
 import net.devh.boot.grpc.server.service.GrpcService;
 import com.manager.estoque.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.text.MessageFormat;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Locale;
 
 @GrpcService
 public class EstoqueGrpcService extends EstoqueServiceGrpc.EstoqueServiceImplBase {
 
-    // Dados padrão para teste
-    private final Map<String, ProdutoEstoque> estoquePadrao = new HashMap<>();
+    private final EstoqueService estoqueService;
 
-    public EstoqueGrpcService() {
-        // Inicializa com dados de exemplo
-        estoquePadrao.put("PROD001", new ProdutoEstoque("PROD001", "Notebook Dell", 50, 2500.00));
-        estoquePadrao.put("PROD002", new ProdutoEstoque("PROD002", "Mouse Logitech", 100, 89.90));
-        estoquePadrao.put("PROD003", new ProdutoEstoque("PROD003", "Teclado Mecânico", 30, 350.00));
-        estoquePadrao.put("PROD004", new ProdutoEstoque("PROD004", "Monitor 24\"", 15, 1200.00));
-        estoquePadrao.put("PROD005", new ProdutoEstoque("PROD005", "Headphone Sony", 25, 450.00));
+    @Autowired
+    public EstoqueGrpcService(EstoqueService estoqueService) {
+        this.estoqueService = estoqueService;
     }
 
     @Override
     public void consultarEstoque(EstoqueProto.ConsultaEstoqueRequest request,
                                  StreamObserver<EstoqueProto.EstoqueResponse> responseObserver) {
 
-        String produtoId = request.getProdutoId();
-        ProdutoEstoque produto = estoquePadrao.get(produtoId);
+        long produtoId = request.getProdutoId();
+        Product produto = estoqueService.findById(produtoId);
 
         EstoqueProto.EstoqueResponse response;
 
         if (produto != null) {
-            response = EstoqueProto.EstoqueResponse.newBuilder()
-                    .setProdutoId(produto.getId())
-                    .setNome(produto.getNome())
-                    .setQuantidade(produto.getQuantidade())
-                    .setPreco(produto.getPreco())
-                    .setStatus("DISPONIVEL")
-                    .setSucesso(true)
-                    .setMensagem("Produto encontrado em estoque")
-                    .build();
+            response = produto.toEstoqueProto("Disponivel", true,
+                    "Produto encontrado em estoque");
         } else {
-            response = EstoqueProto.EstoqueResponse.newBuilder()
-                    .setProdutoId(produtoId)
-                    .setNome("Produto não cadastrado")
-                    .setQuantidade(0)
-                    .setPreco(0.0)
-                    .setStatus("NAO_ENCONTRADO")
-                    .setSucesso(false)
-                    .setMensagem("Produto não encontrado no estoque")
-                    .build();
+            response = createErrorResponse(produtoId, "Produto não encontrado no estoque");
         }
 
         responseObserver.onNext(response);
@@ -64,17 +45,17 @@ public class EstoqueGrpcService extends EstoqueServiceGrpc.EstoqueServiceImplBas
     public void listarTodosEstoques(EstoqueProto.ListaEstoquesRequest request,
                                     StreamObserver<EstoqueProto.ListaEstoquesResponse> responseObserver) {
 
-        List<EstoqueProto.EstoqueResponse> produtos = estoquePadrao.values().stream()
+        List<EstoqueProto.EstoqueResponse> produtos = estoqueService.findAllProducts().stream()
                 .map(produto -> EstoqueProto.EstoqueResponse.newBuilder()
                         .setProdutoId(produto.getId())
-                        .setNome(produto.getNome())
-                        .setQuantidade(produto.getQuantidade())
-                        .setPreco(produto.getPreco())
+                        .setNome(produto.getName())
+                        .setQuantidade(produto.getUnitsInStock())
+                        .setPreco(produto.getUnitPrice() != null ? produto.getUnitPrice() : 0.0f)
                         .setStatus("DISPONIVEL")
                         .setSucesso(true)
                         .setMensagem("")
                         .build())
-                .collect(Collectors.toList());
+                .toList();
 
         EstoqueProto.ListaEstoquesResponse response = EstoqueProto.ListaEstoquesResponse.newBuilder()
                 .addAllProdutos(produtos)
@@ -90,76 +71,58 @@ public class EstoqueGrpcService extends EstoqueServiceGrpc.EstoqueServiceImplBas
     public void atualizarEstoque(EstoqueProto.AtualizaEstoqueRequest request,
                                  StreamObserver<EstoqueProto.EstoqueResponse> responseObserver) {
 
-        String produtoId = request.getProdutoId();
+        int produtoId = request.getProdutoId();
         int quantidade = request.getQuantidade();
         String operacao = request.getOperacao();
 
-        ProdutoEstoque produto = estoquePadrao.get(produtoId);
+        Product produto = estoqueService.findById((long) produtoId);
 
         EstoqueProto.EstoqueResponse response;
 
         if (produto != null) {
-            int novaQuantidade = produto.getQuantidade();
-            String mensagem = "";
-            boolean sucesso = true;
+            int novaQuantidade = produto.getUnitsInStock();
+            int qtdOriginal = quantidade;
 
-            if ("ENTRADA".equals(operacao)) {
-                novaQuantidade += quantidade;
-                mensagem = "Entrada de " + quantidade + " unidades realizada";
-            } else if ("SAIDA".equals(operacao)) {
-                if (produto.getQuantidade() >= quantidade) {
-                    novaQuantidade -= quantidade;
-                    mensagem = "Saída de " + quantidade + " unidades realizada";
-                } else {
-                    sucesso = false;
-                    mensagem = "Estoque insuficiente. Disponível: " + produto.getQuantidade();
+            if ("SAIDA".equalsIgnoreCase(operacao)) {
+                if (produto.getUnitsInStock() < quantidade) {
+                    response = createErrorResponse(produtoId,
+                            "Estoque insuficiente. Disponível: " + produto.getUnitsInStock());
+                    sendResponse(response, responseObserver);
+                    return;
                 }
+                quantidade = -quantidade;
             }
 
-            if (sucesso) {
-                // Atualiza a quantidade (em memória)
-                produto.setQuantidade(novaQuantidade);
-            }
+            novaQuantidade += quantidade;
+            produto.setUnitsInStock((short) novaQuantidade);
+            estoqueService.save(produto);
 
-            response = EstoqueProto.EstoqueResponse.newBuilder()
-                    .setProdutoId(produto.getId())
-                    .setNome(produto.getNome())
-                    .setQuantidade(produto.getQuantidade())
-                    .setPreco(produto.getPreco())
-                    .setStatus(sucesso ? "ATUALIZADO" : "ERRO")
-                    .setSucesso(sucesso)
-                    .setMensagem(mensagem)
-                    .build();
+            String mensagem = MessageFormat.format("{0} de {1} unidades realizada",
+                    operacao.toLowerCase(), qtdOriginal);
+
+            response = produto.toEstoqueProto("Disponível", true, mensagem);
         } else {
-            response = EstoqueProto.EstoqueResponse.newBuilder()
-                    .setProdutoId(produtoId)
-                    .setNome("Produto não cadastrado")
-                    .setQuantidade(0)
-                    .setPreco(0.0)
-                    .setStatus("NAO_ENCONTRADO")
-                    .setSucesso(false)
-                    .setMensagem("Produto não encontrado para atualização")
-                    .build();
+            response = createErrorResponse(produtoId, "Produto não encontrado para atualização");
         }
 
+        sendResponse(response, responseObserver);
+    }
+
+
+    private void sendResponse(EstoqueProto.EstoqueResponse response, StreamObserver<EstoqueProto.EstoqueResponse> responseObserver) {
         responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
 
-    // Classe interna para representar o produto - VERSÃO CORRIGIDA
-    @Getter
-    @Setter
-    private static class ProdutoEstoque {
-        private String id;
-        private String nome;
-        private int quantidade;
-        private double preco;
-
-        public ProdutoEstoque(String id, String nome, int quantidade, double preco) {
-            this.id = id;
-            this.nome = nome;
-            this.quantidade = quantidade;
-            this.preco = preco;
-        }
+    private EstoqueProto.EstoqueResponse createErrorResponse(long produtoId, String mensagem) {
+        return EstoqueProto.EstoqueResponse.newBuilder()
+                .setProdutoId((int) produtoId)
+                .setNome("")
+                .setQuantidade(0)
+                .setPreco(0.0f)
+                .setStatus("NAO_ENCONTRADO")
+                .setSucesso(false)
+                .setMensagem(mensagem)
+                .build();
     }
 }
